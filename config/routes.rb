dcc3e809930cbc5782514683d22059beb2882a9a
Alfred M <alfred.mkg@proton.me>
2023-11-22 01:50:07 +0300

    Add random greeting api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'root/index'
 
+  namespace :api do
+    get 'random_greeting' => 'greetings#random_greeting'
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "root#index"
