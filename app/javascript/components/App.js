615def20942453d354cb39533d5aad47afec5e95
Alfred M <alfred.mkg@proton.me>
2023-11-26 19:53:15 +0300

    Add greeting component

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Greeting from './Greeting';
 
 const App = () => {
   return (
-    <h1>Random greeting inside this app component</h1>
+    <>
+      <h1>Random greeting inside this app component</h1>
+      <Greeting />
+    </>
   )
 }
 export default App
---
+++ app/javascript/components/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Greeting = () => {
+  return (
+    <div>This is the greeting component</div>
+  )
+}
+export default Greeting
