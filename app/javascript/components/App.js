0ebecfe5a5aedb626c43a06c63f8bf1554c396c1
Alfred M <alfred.mkg@proton.me>
2023-11-26 22:30:18 +0300

    Get random greeting from api

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route} from 'react-router-dom';
 import Greeting from './Greeting';
+import { useDispatch } from 'react-redux';
+import { getGreeting } from '../greeting/greetingSlice';
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getGreeting())
+  }, [])
   return (
     <main>
       <Routes>
--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Greeting = () => {
+  const { greeting, isLoading, isError, errorMsg } = useSelector((store) => store.greeting);
+
+  console.log(greeting.message);
+
+  if (isLoading) {
+    return (<h1>Loading Greeting</h1>);
+  }
+
+  if (isError) {
+    return (<h1>{ errorMsg }</h1>);
+  }
+
   return (
-    <h1>This is the greeting component</h1>
+    <h1>{ greeting.message }</h1>
   )
 }
 export default Greeting
