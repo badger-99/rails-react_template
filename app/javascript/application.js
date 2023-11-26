95fc7ed8cf1571fb23f9b7b94bf8dc6e6ca3a861
Alfred M <alfred.mkg@proton.me>
2023-11-26 22:29:58 +0300

    Configure store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
